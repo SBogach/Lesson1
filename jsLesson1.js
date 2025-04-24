82e555ce8ea0dc2eafae651ac26534b499e9c113
SBogach <S.Bogach@compassplus.com>
2025-04-24 16:21:14 +0500

    Create jsLesson1.js

---
+++ jsLesson1.js
@@ -0,0 +1,13 @@
+let numberVar = 5; //Создали переменную numberVar со значением 5
+const stringVar = "stringVarValue"; // Создали переменную stringVar со значением "stringVarValue"
+console.log(numberVar); // Вывели переменную numberVar
+console.log(stringVar); // Вывели переменную stringVar
+console.log(numberVar, stringVar); // Вывели обе переменные 
+
+
+/* Изменили значение numberVar
+после чего вывели переменные снова */
+numberVar = 5 * 2;
+console.log(numberVar);
+console.log(stringVar);
+console.log(numberVar, stringVar);
